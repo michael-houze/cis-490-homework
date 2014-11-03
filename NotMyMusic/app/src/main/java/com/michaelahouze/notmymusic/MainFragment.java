ef033b150ca508f9227138dd55aaa8b7a15cc5c8
Michael Houze <michael.houze@gmail.com>
2014-11-02 21:51:32 -0500

    Homework 5 - Not My Music Layout
    - Set up the project
    - Added images
    - Created GridView, Fragment and Adapter
    - Added toasts when image gets clicked
    - Still need to call detail fragment
    - Still need to add toolbar options

---
+++ NotMyMusic/app/src/main/java/com/michaelahouze/notmymusic/MainFragment.java
@@ -0,0 +1,29 @@
+package com.michaelahouze.notmymusic;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+public class MainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        GridView gridView = (GridView) view.findViewById(R.id.gridView);
+        gridView.setAdapter(new GridAdapter(getActivity()));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getActivity(), "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return view;
+    }
+}
